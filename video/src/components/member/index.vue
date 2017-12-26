b28b40547af913ee40adff929422ae900001365b
3k-Grubabby <wc549476585@outlook.com>
2017-12-26 16:57:59 +0800

    upage

---
+++ video/src/components/member/index.vue
@@ -0,0 +1,32 @@
+<template>
+<div id="outbox">
+    <el-row>
+      <el-col :span="3">
+          <membermenu></membermenu>
+
+      </el-col>
+        <el-col :span="21">
+            <router-view>
+
+            </router-view>
+          
+      </el-col>
+    </el-row>
+
+</div>
+</template>
+<style>
+  #outbox{
+    width: 80%;
+    margin: 50px auto;
+  }
+
+</style>
+<script>
+import membermenu from "./menu.vue";
+export default {
+  components:{
+      membermenu
+  }
+}
+</script>
